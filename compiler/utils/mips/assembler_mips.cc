61d6d842e30ce4cf037515b3204d129f37ee3d44
Alexey Frunze <Alexey.Frunze@imgtec.com>
2016-07-19 15:00:40 -0700

    MIPS32: Improve string and class loads
    Tested:
    - MIPS32 Android boots in QEMU
    - test-art-host-gtest
    - test-art-target-run-test-optimizing in QEMU, on CI20
    - test-art-target-gtest on CI20
    
    Change-Id: I70fd5d5267f8594c3b29d5a4ccf66b8ca8b09df3
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- compiler/utils/mips/assembler_mips.cc
+++ compiler/utils/mips/assembler_mips.cc
@@ -2006,6 +2006,10 @@ void MipsAssembler::BindPcRelBaseLabel() {
   Bind(&pc_rel_base_label_);
 }
 
+uint32_t MipsAssembler::GetPcRelBaseLabelLocation() const {
+  return GetLabelLocation(&pc_rel_base_label_);
+}
+
 void MipsAssembler::FinalizeLabeledBranch(MipsLabel* label) {
   uint32_t length = branches_.back().GetLength();
   if (!label->IsBound()) {
